3dd2c1c257d429e95a52df9a25a798d0ef83483d
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2020-05-29 11:41:25 +0530

    Added required files
    1. Bhuvan_Probability
           __init__.py
           Binomialdistribution.py
           Gaussiandistribution.py
           Generaldistribution.py
           license.txt
           README.md
           setup.cfg
    
    2. setup.py

---
+++ Python Package(PyPi) - bhuvan_probability/Bhuvan_Probability/README.md
@@ -0,0 +1,2 @@
+# Bhuvan_Probability
+A PyPi package for Gaussian Distribution and Binomial Distribution 
---
+++ Python Package(PyPi) - bhuvan_probability/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bhuvan_probability',
+      version='1.1',
+      description='Gaussian  and Binomial distributions',      
+      packages=['bhuvan_probability'],
+      author = 'Gangadhar Bhuvan',
+      author_email = 'gangadharbhuvan1@gmail.com',
+    zip_safe=False)
